fd53c3c414e8c54d937e0e2d8af67e2a59635b6b
Sondre <hello@sondreg.net>
2021-03-06 12:28:25 +0100

    minor changes to Footer and package.json

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 
+const currentTime = new Date();
+const currentYear = currentTime.getFullYear();
+
+
 function Footer() {
     return(
         <footer className="absolute text-center -bottom-0 w-full h-10 ">
-            <p className="text-gray-300">Copyright &copy; 2021</p>
+            <p className="text-gray-300">Copyright &copy; {currentYear}</p>
         </footer>
     );
 }
